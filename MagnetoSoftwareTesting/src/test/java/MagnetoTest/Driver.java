0cd60cd9f142c832d2a92f1881ec5cf0acbcdd76
Rajashree Sonawane <rajashrees@synerzipune.local>
2023-02-14 15:51:41 +0530

    Resolved all comments

--- MagnetoSoftwareTesting/src/test/java/MagnetoTest/Driver.java
+++ MagnetoSoftwareTesting/src/test/java/MagnetoTest/Driver.java
@@ -2,38 +2,45 @@ package MagnetoTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.apache.log4j.Logger;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
-	
+
+	static Logger log = Logger.getLogger(Driver.class);
 	WebDriver driver;
 	String browser;
-	public Driver(String browser)
-	{
-		this.browser=browser;
+
+	public Driver(String browser) {
+		this.browser = browser;
 	}
-	public WebDriver setDriver() 
-	{
-		if(browser.equalsIgnoreCase("firefox"))
-		{
-			System.out.println("Executing on FireFox");
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			}
-		else if (browser.equalsIgnoreCase("chrome")) 
-		{System.out.println("Executing on CHROME");
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();}
-		else if (browser.equalsIgnoreCase("ie")) 
-		{
-			System.out.println("Executing on IE");
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
+
+	public WebDriver setDriver() {
+		if (browser.equalsIgnoreCase("firefox")) {
+			log.info("Executing on FireFox");
+			FirefoxOptions options=new FirefoxOptions();
+			options.setHeadless(true);
+			driver=new FirefoxDriver(options);
+			
+			} else if (browser.equalsIgnoreCase("chrome")) {
+			log.info("Executing on CHROME");
+			ChromeOptions options=new ChromeOptions();
+			options.addArguments("headless");
+			driver=new ChromeDriver(options);
+			
+			} else if (browser.equalsIgnoreCase("ie")) {
+			log.info("Executing on IE");
+			EdgeOptions options=new EdgeOptions();
+			options.setHeadless(true);
+			driver=new EdgeDriver(options);
 			}
 		return driver;
-			}
+	}
 
 }
--- MagnetoSoftwareTesting/src/test/java/MagnetoTest/MagnetoApplicationTest.java
+++ MagnetoSoftwareTesting/src/test/java/MagnetoTest/MagnetoApplicationTest.java
@@ -1,164 +1,308 @@
 package MagnetoTest;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.time.Duration;
+
+import java.util.Properties;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.AfterTest;
+
 import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
-
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class MagnetoApplicationTest {
-public String Url ="https://magento.softwaretestingboard.com";  
-//String driverpath ="C:\\Users\\rajashrees\\Desktop\\Seljars\\chromedriver_win32 (4)\\chromedriver.exe"; 
-//String driverName = "webdriver.chrome.driver";  
-public WebDriver driver; 
 
-@Parameters("browser")
-@BeforeTest  
-public void browser(String browser)  
-{	  
-//System.setProperty(driverName,driverpath);  
-//driver= new ChromeDriver();	
-Driver d=new Driver(browser);
-driver=d.setDriver();
-driver.manage().window().maximize();     
-driver.get(Url);  
-driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);  
-}	  
+	WebDriver driver;
 
-@Test(priority = 0)  
-public void chooseProduct() throws InterruptedException 
-{  
-    Actions act = new Actions(driver);
-    
-	WebElement Women =driver.findElement(By.xpath("//*[@id=\"ui-id-4\"]/span[2]"));
-	act.moveToElement(Women).perform();
-	Thread.sleep(1000);
-	WebElement Tops =driver.findElement(By.xpath("//*[@id=\"ui-id-9\"]/span[2]"));
-	act.moveToElement(Tops).perform();
-	Thread.sleep(1000);
-	WebElement Tees =driver.findElement(By.xpath("//*[@id=\"ui-id-13\"]"));
-	Tees.click();
-	System.out.println("choose Women Top,Tees product and clicked");
-	
-}  
+	WebDriverWait wait;
 
-@Test(priority = 1)  
-public void selectProduct() 
-{  
-   driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[3]/div[1]/div[3]/ol/li[7]/div/a/span/span/img")).click();
-}  
+	static Logger log = Logger.getLogger(MagnetoApplicationTest.class);
 
-@Test(priority = 2)  
-public void AddToCart() throws InterruptedException 
-{  
-   
-	driver.findElement(By.xpath("//*[@id=\"option-label-size-143-item-166\"]")).click(); //select size
-	driver.findElement(By.xpath("//*[@id=\"option-label-color-93-item-58\"]")).click(); //select color
-	Thread.sleep(2000);
-	driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]/span")).click(); //click on add to cart button
-	boolean addedcart= driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]/span")).isDisplayed();
-    Assert.assertTrue(addedcart);
-    
-} 
+	public static Properties get_properties_Data() throws Exception {
 
-@Test(priority = 3)  
-public void Shippingcart() throws InterruptedException
-{  
-  Thread.sleep(2000); 
-  driver.findElement(By.xpath("//header/div[2]/div[1]/a[1]")).click(); //click on shipping cart
-  Thread.sleep(2000);
-  driver.findElement(By.xpath("//button[@title='Proceed to Checkout']")).click(); //click on proceed to checkout
-} 
+		FileReader reader = new FileReader("src\\test\\java\\ConfigProperties\\url.properties");
+		Properties url = new Properties();
+		try {
+			url.load(reader);
+		} catch (IOException e) {
 
-@Test(priority = 4)  
-public void FillShippingData() throws InterruptedException
-{  
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//body/div[2]/main[1]/div[2]/div[1]/div[2]/div[4]/ol[1]/li[1]/div[2]/form[1]/fieldset[1]/div[1]/div[1]/input[1]")).sendKeys("Rajashree.sonawane@gmail.com");
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//input[@name=\"firstname\"]")).sendKeys("Rajashree");
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Sonawane");
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//input[@name='company']")).sendKeys("Excellarate");
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//input[@name='street[0]']")).sendKeys("Handewadi road");
-  Thread.sleep(1000); 
-  driver.findElement(By.xpath("//input[@name='city']")).sendKeys("Pune");
-  Thread.sleep(2000);
-  driver.findElement(By.xpath("/html[1]/body[1]/div[2]/main[1]/div[2]/div[1]/div[2]/div[4]/ol[1]/li[1]/div[2]/form[2]/div[1]/div[5]/div[1]/select[1]")).click();
-  Thread.sleep(2000); 
-  driver.findElement(By.xpath("//*[@data-title=\'Alabama\']")).click();
-  Thread.sleep(1000);
-  driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys("24365-1587");
-  Thread.sleep(1000); 
-   driver.findElement(By.xpath("/html[1]/body[1]/div[2]/main[1]/div[2]/div[1]/div[2]/div[4]/ol[1]/li[1]/div[2]/form[2]/div[1]/div[8]/div[1]/select[1]")).click();
-   Thread.sleep(1000);
-   driver.findElement(By.xpath("//*[@data-title=\'Afghanistan\']")).click();
-   Thread.sleep(1000);
-  driver.findElement(By.xpath("//input[@name='telephone']")).sendKeys("9657344065");
-  Thread.sleep(1000);
-  driver.findElement(By.xpath("//input[@name='ko_unique_5']")).click();
-  Thread.sleep(1000);
-  driver.findElement(By.xpath("//*[@id=\"shipping-method-buttons-container\"]/div/button")).click();
-  
-} 
+			log.error("Ops!", e);
 
-@Test(priority = 5)  
-public void VerifyOrderSummary() throws InterruptedException
-{  
-  Thread.sleep(5000); 
-  driver.findElement(By.xpath("//*[@id='opc-sidebar']/div[1]/div/div[1]")).click();
-  WebElement Product_name =driver.findElement(By.xpath("//strong[contains(text(),'Minerva LumaTech™ V-Tee')]"));
-  String ExpectedText = "Minerva LumaTech™ V-Tee";
-  System.out.println(Product_name.getText());
-  Assert.assertEquals(ExpectedText, Product_name.getText());
-  System.out.println("Product_name text is a expected – Assert passed");
-  
-  WebElement Product_Quantity =driver.findElement(By.xpath("//div[@class='product-item-name-block']/div/span[2]"));
-  String ExpectedText1 = "1";
-  System.out.println(Product_Quantity.getText());
-  Assert.assertEquals(ExpectedText1, Product_Quantity.getText());
-  System.out.println(" Product_Quantity text is a expected – Assert passed");
-  
-  WebElement Product_price =driver.findElement(By.xpath("//span[contains(text(),'$32.00')]"));
-  String ExpectedText2 = "$32.00";
-  System.out.println(Product_price.getText());
-  Assert.assertEquals(ExpectedText2, Product_price.getText());
-  System.out.println("Product_price text is a expected – Assert passed");
-  
-  driver.findElement(By.xpath("//*[@id=\"opc-sidebar\"]/div[1]/div/div[2]/div/ol/li/div/div/div[2]/span/span")).click();
-  
-  		  
-  
-  	WebElement Product_size =driver.findElement(By.xpath("//dd[contains(text(),'XS')]"));
-  	String ExpectedText3 = "XS";
-  	System.out.println(Product_size.getText());
-  	Assert.assertEquals(ExpectedText3, Product_size.getText());
-  System.out.println(" Product_size text is a expected – Assert passed");
-  		  
-  
-  WebElement Product_color =driver.findElement(By.xpath("//dd[contains(text(),'Red')]"));
-  String ExpectedText4 = "Red";
-  System.out.println(Product_color.getText());
-  Assert.assertEquals(ExpectedText4, Product_color.getText());
-  System.out.println("Product_color text is a expected – Assert passed");
-  		  
-  		  		  
-  
-} 
+		}
+		return url;
 
-@AfterTest  
-public void Close() 
-{  
-driver.quit();  
-}  
+	}
+
+	/**
+	 * Code is Functional testing application
+	 * 
+	 * @param browser (Code is executed in cross browser like chrome,edge and
+	 *                firefox)
+	 * @return
+	 * @throws Exception
+	 */
+	@Parameters("browser")
+	@BeforeTest
+	public void browser(String browser) throws Exception {
+		PropertyConfigurator.configure("src\\main\\resources\\log4j.properties");
+		Driver d = new Driver(browser);
+		driver = d.setDriver();
+		driver.manage().window().maximize();
+		driver.get(get_properties_Data().getProperty("url"));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		log.info("Open the Magneto software Testing Url");
+
+	}
+
+	/**
+	 * @param chooseProduct
+	 * @param mouse         Hover(perform actions for women,tops and tees)
+	 * @param tees
+	 * @return
+	 * @throws InterruptedException
+	 */
+	@Test(priority = 0)
+	public void chooseProduct() throws InterruptedException {
+
+		Actions act = new Actions(driver);
+
+		By womenBy = By.xpath("//li[@class='level0 nav-2 category-item level-top parent ui-menu-item']/a");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(womenBy));
+		WebElement women = driver.findElement(womenBy);
+		act.moveToElement(women).build().perform();
+		log.info("Move to Women");
+
+		By topsBy = By.xpath("//*[@id='ui-id-9']/span[2]");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(topsBy));
+		WebElement tops = driver.findElement(topsBy);
+		act.moveToElement(tops).perform();
+		log.info("move to Tops");
+
+		By teesBy = By.xpath("//*[@id='ui-id-13']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(teesBy)).click();
+		log.info("move to tees and clicked tees product");
+		log.info("choose Women Top,Tees product and clicked on product");
+
+	}
+
+	/**
+	 * @param selectProduct
+	 * @param Minerva       LumaTech™ V-Tee
+	 * @return
+	 */
+
+	@Test(priority = 1)
+	public void selectProduct() {
+		By selectproductBy = By.xpath("//ol[@class='products list items product-items']/li[7]");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(selectproductBy)).click();
+		log.info("clicked on Minerva LumaTech™ V-Tee product");
+	}
+
+	/**
+	 * @param Add        to cart selected product
+	 * @param size,color and quantity
+	 * @return
+	 */
+	@Test(priority = 2)
+	public void AddToCart() {
+
+		By sizeBy = By.xpath("//div[@aria-label='XS']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(sizeBy)).click();
+		log.info("click on 'size' and select size");
+
+		By colorBy = By.xpath("//div[@aria-label='Red']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(colorBy)).click();
+		log.info("click on 'color' and select color");
+
+		By AddedCartBy = By.xpath("//button[@class='action primary tocart']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(AddedCartBy)).click();
+		log.info("clicked on Add to cart button");
+
+		By msg = By.xpath("//div[@role='alert']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(msg));
+
+	}
+
+	/**
+	 * @param show    selected product
+	 * @param Add     to cart
+	 * @param proceed to checkout
+	 * @return
+	 */
+	@Test(priority = 3)
+	public void Shippingcart() {
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By showcartBy = By.xpath("//a[@class='action showcart']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(showcartBy)).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		log.info("click on show cart button");
+
+		By checkoutBy = By.xpath("//button[@id='top-cart-btn-checkout']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(checkoutBy)).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		log.info("click on proceed to checkout");
+
+	}
+
+	/**
+	 * @param filling    shipping data
+	 * @param data(Email id,first name,last name,company,street address,city)
+	 * @param data       (state,post code,country,telephone number and shipping
+	 *                   methods)
+	 * @return
+	 * @throws Exception
+	 */
+	@Test(priority = 4)
+
+	public void FillShippingData() throws Exception {
+
+		By emailBy = By.xpath("//fieldset[@id='customer-email-fieldset']//input[@type='email']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(emailBy))
+				.sendKeys(get_properties_Data().getProperty("email"));
+		log.info("Enter Email id");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By firstnameBy = By.xpath("//input[@name='firstname']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(firstnameBy))
+				.sendKeys(get_properties_Data().getProperty("firstname"));
+		log.info("enter FirstName");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By lastnameBy = By.xpath("//input[@name='lastname']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(lastnameBy))
+				.sendKeys(get_properties_Data().getProperty("lastname"));
+		log.info("enter lastname");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By companyBy = By.xpath("//input[@name='company']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(companyBy))
+				.sendKeys(get_properties_Data().getProperty("company"));
+		log.info("enter company name");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By streetBy = By.xpath("//input[@name='street[0]']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(streetBy))
+				.sendKeys(get_properties_Data().getProperty("street_address"));
+		log.info("enter street address");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By cityBy = By.xpath("//input[@name='city']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(cityBy))
+				.sendKeys(get_properties_Data().getProperty("city"));
+		log.info("enter city");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By postcodeBy = By.xpath("//input[@name='postcode']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(postcodeBy))
+				.sendKeys(get_properties_Data().getProperty("postal_code"));
+		log.info("enter postcode");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By countryBy = By.xpath("//select[@name='country_id']//option[@data-title='India']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(countryBy)).click();
+		log.info("click on country");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By regionBy = By.xpath("//select[@name='region_id']//option[@data-title='Andhra Pradesh']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(regionBy)).click();
+		log.info("click on state");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By phoneBy = By.xpath("//input[@name='telephone']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(phoneBy))
+				.sendKeys(get_properties_Data().getProperty("phone_no"));
+		log.info("enter mobile number");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By keymethodBy = By.xpath("//*[@id='label_carrier_flatrate_flatrate']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(keymethodBy)).click();
+		log.info("click on shipping methods");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By nextbuttonBy = By.xpath("//button[@class='button action continue primary']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(nextbuttonBy)).click();
+		log.info("click on next button");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+	}
+
+	/**
+	 * @param Verify                         order summary
+	 * @param product_name
+	 * @param product_quantity,product_color ,product_size and product_price
+	 * @return
+	 */
+
+	@Test(priority = 5)
+	public void VerifyOrderSummary() {
+		WebElement ordersummary = driver.findElement(By.xpath("//div[@class='opc-block-summary']"));
+		wait.until(ExpectedConditions.visibilityOf(ordersummary));
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		By productdetailBy = By.xpath("//div[@class='block items-in-cart']/div");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productdetailBy));
+		WebElement product = driver.findElement(productdetailBy);
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].click();", product);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By nameBy = By.xpath("//div[@class='product-item-name-block']/strong[@class='product-item-name']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(nameBy));
+		WebElement Product_name = driver.findElement(nameBy);
+		String ExpectedText = "Minerva LumaTech™ V-Tee";
+		log.info(Product_name.getText());
+		Assert.assertEquals(ExpectedText, Product_name.getText());
+		log.info("Product_name text is a expected – Assert passed");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By quantityBy = By.xpath("//div[@class='product-item-name-block']/div/span[2]");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(quantityBy));
+		WebElement Product_Quantity = driver.findElement(quantityBy);
+		String ExpectedText1 = "1";
+		Assert.assertEquals(ExpectedText1, Product_Quantity.getText());
+		log.info(" Product_Quantity text is a expected – Assert passed");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+		By priceBy = By.xpath("//span[@class='cart-price']");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(priceBy));
+		WebElement Product_price = driver.findElement(priceBy);
+		String ExpectedText2 = "$32.00";
+		Assert.assertEquals(ExpectedText2, Product_price.getText());
+		log.info("Product_price text is a expected – Assert passed");
+
+	}
+
+	/**
+	 * automated all test cases and close the driver
+	 */
+
+	@AfterTest
+	public void Close() {
+
+		log.info("Broswer Close");
+		driver.quit();
+
+	}
 }
